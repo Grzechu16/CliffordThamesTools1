bd31ac321f4c98079935d2f2e07b836549026011
Your Name <grzegorzkijas@outlook.com>
2018-03-02 00:00:51 +0100

    comments added

--- src/fullFile/Controller.java
+++ src/fullFile/Controller.java
@@ -7,32 +7,26 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.*;
 import java.util.ArrayList;
 
 public class Controller {
 
-    JFileChooser fileChooser;
-    FileNameExtensionFilter extensionFilter;
     FileReader fileReader;
     BufferedReader bufferedReader;
     private String finis;
-    private final String outPath = "C:\\Users\\DELL\\Desktop\\FullFileTest\\FindRaport.csv";
-    private String finisInRow;
-    private String marketInRow;
-    public CheckBox[] checkBoxes;
-    public  String[] marketTab;
+   // private final String outPath = "C:\\Users\\DELL\\Desktop\\FullFileTest\\FindRaport.csv";
+   private final String outPath = "FindRaport.csv";
+    private boolean allSelected = false;
     private ArrayList<CheckBox> layoutMarketCheckBox;
     private ArrayList<String> marketShortcut;
+    private int marketCount, lineCount;
 
     @FXML
     CheckBox axCheck, bxCheck, chCheck, csCheck, dkCheck, dxCheck, exCheck, fxCheck, gbCheck, grCheck,
             hxCheck, irCheck, ixCheck, nlCheck, nxCheck, plCheck, pxCheck, ruCheck, roCheck, sfCheck, sxCheck;
     @FXML
-    Button findButton, restartButton, exitButton;
+    Button findButton, restartButton, exitButton, selectAll;
     @FXML
     TextField finisTextField;
 
@@ -41,49 +35,16 @@ public class Controller {
         createList();
     }
 
-    /** Method checks if finis textField are empty */
-    public void checkIfEmpty() throws IOException {
-        if ((finisTextField.getText().equals(""))) {
-            showAlert("Specify part number first");
-        }
-
-    }
-
-    /** Method initialize process of searching through price file */
-    public void readFile(String path) throws IOException {
-        finis = finisTextField.getText().toString();
-        createNewFile();
-
-        String row = "";
-        boolean isFinisFound = false;
-
+    /**
+     * Method creates new file with column labels
+     */
+    public void createNewFile() {
         try {
-            fileReader = new FileReader(path);
-            bufferedReader = new BufferedReader(fileReader);
-            FileWriter fileWriter = new FileWriter("C:\\Users\\DELL\\Desktop\\FullFileTest\\FindRaport.csv");
-            BufferedWriter buff2 = new BufferedWriter(fileWriter);
-
-            while ((row = bufferedReader.readLine()) != null) {
-                if (row.toUpperCase().contains(finis.toUpperCase())) {
-                buff2.write(row);
-                buff2.newLine();
-                }
-            }
-            bufferedReader.close();
-            buff2.close();
-        } catch (Exception e1) {
-            showAlert("Invalid or missing file");
-            e1.printStackTrace();
-        }
-    }
-
-    public void createNewFile(){
-        try {
-            FileWriter fileWriter = new FileWriter("C:\\Users\\DELL\\Desktop\\FullFileTest\\FindRaport.csv");
-            BufferedWriter buff2 = new BufferedWriter(fileWriter);
-            buff2.write("Searched finis:" + "," + finis);
-            buff2.newLine();
-            buff2.write("Vehicle Line" + "," + "FCJ Menu Number" + "," + "Operation (EN)" + "," + "Year Dates   " + ","
+            FileWriter fileWriter = new FileWriter("FindRaport.csv");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write("Searched finis:" + "," + finis);
+            bufferedWriter.newLine();
+            bufferedWriter.write("Vehicle Line" + "," + "FCJ Menu Number" + "," + "Operation (EN)" + "," + "Year Dates   " + ","
                     + "Variant (EN)" + "," + "Part Description (EN)" + "," + "FINIS" + "," + "MLI" + ","
                     + "Parts Quantity" + "," + "Parts Unit Price excl. VAT" + "," + "Parts Retail Excl. VAT" + ","
                     + "Parts Retail Incl. VAT" + "," + "Labour Time" + "," + "Labour Cost Excl. VAT " + ","
@@ -91,8 +52,8 @@ public class Controller {
                     + "Fleet National Pricing Region 1" + "," + "Fleet National Pricing Region 2" + ","
                     + "Fleet National Pricing Region 3" + "," + "Fleet National Pricing Region 4" + ","
                     + "Ford Motorcraft 4+(Retail)" + "," + "Recommended Fitted Price(Retail)");
-            buff2.newLine();
-            buff2.close();
+            bufferedWriter.newLine();
+            bufferedWriter.close();
 
         } catch (IOException e2) {
             // TODO Auto-generated catch block
@@ -100,7 +61,81 @@ public class Controller {
         }
     }
 
-    /** Method allows to show information alerts */
+    /**
+     * Method search thru selected market files and marks label on Green when file exists and on Red when file is unavailable
+     */
+    public void readSelectedMarkets() {
+        checkIfFileExist();
+
+        if ((finisTextField.getText()) != "") {
+            for (int i = 0; i < layoutMarketCheckBox.size(); i++) {
+                if (layoutMarketCheckBox.get(i).isSelected()) {
+                    layoutMarketCheckBox.get(i).setDisable(false);
+                    readFile(marketShortcut.get(i) + ".csv", layoutMarketCheckBox.get(i));
+                } else
+                    layoutMarketCheckBox.get(i).setDisable(true);
+            }
+            showAlert("Lines checked: " + lineCount + "\n" + "Occurrences of part: " + marketCount);
+            lineCount = 0;
+            marketCount = 0;
+        } else
+            showAlert("Specify part number first");
+    }
+
+    /**
+     * Method initialize process of searching through price file
+     */
+    public void readFile(String path, CheckBox layoutMarketCheckBox) {
+        finis = finisTextField.getText().toString();
+
+        String row = "";
+
+        try {
+            fileReader = new FileReader(path);
+            bufferedReader = new BufferedReader(fileReader);
+            FileWriter fileWriter = new FileWriter("FindRaport.csv", true);
+            BufferedWriter buff2 = new BufferedWriter(fileWriter);
+
+            while ((row = bufferedReader.readLine()) != null) {
+                lineCount++;
+                if (row.toUpperCase().contains(finis.toUpperCase())) {
+                    buff2.append(row);
+                    buff2.newLine();
+                    buff2.flush();
+                    marketCount++;
+                }
+            }
+            layoutMarketCheckBox.setTextFill(Color.GREEN);
+            bufferedReader.close();
+            buff2.close();
+        } catch (Exception e1) {
+            layoutMarketCheckBox.setTextFill(Color.RED);
+            e1.printStackTrace();
+        }
+    }
+
+    /**
+     * Method checks whether previous raport exists and informs user about its deletion
+     */
+    public void checkIfFileExist() {
+        File file = new File(outPath);
+        if (file.exists()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "", ButtonType.OK);
+            alert.setHeaderText("Previous raport exist. Click OK to delete it and proceed with new search");
+            alert.showAndWait();
+            if (alert.getResult() == ButtonType.OK) {
+                file.delete();
+                alert.close();
+                createNewFile();
+            }
+        } else {
+            createNewFile();
+        }
+    }
+
+    /**
+     * Method allows to show information alerts
+     */
     public void showAlert(String text) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION, "", ButtonType.OK);
         alert.setHeaderText(text);
@@ -110,7 +145,9 @@ public class Controller {
         }
     }
 
-    /** Method creates ArrayList of market labels (javafx elements) and ArrayList with markets shortcuts */
+    /**
+     * Method creates ArrayList of market labels (javafx elements) and ArrayList with markets shortcuts
+     */
     private void createList() {
         layoutMarketCheckBox = new ArrayList<>();
         marketShortcut = new ArrayList<>();
@@ -127,25 +164,29 @@ public class Controller {
             marketShortcut.add(s);
     }
 
-    public void checkSelectedMarkets() throws IOException {
-        if((finisTextField.getText())!=""){
-            createList();
-            createNewFile();
-            for(int i=0 ; i<layoutMarketCheckBox.size();i++){
-                if(layoutMarketCheckBox.get(i).isSelected()){
-                    layoutMarketCheckBox.get(i).setDisable(false);
-                    readFile("C:\\Users\\DELL\\Desktop\\FullFileTest\\"+marketShortcut.get(i)+".csv");
-                }
-                else
-                    layoutMarketCheckBox.get(i).setDisable(true);
+    /**
+     * Method allows to select/deselect all markets for search
+     */
+    public void selectAllMarkets(){
+
+        if(allSelected!=true){
+            for(int i=0;i<layoutMarketCheckBox.size();i++){
+                layoutMarketCheckBox.get(i).setSelected(true);
             }
+            allSelected=true;
         }
-        else
-            showAlert("Type finis first!");
+        else{
+            for(int i=0;i<layoutMarketCheckBox.size();i++){
+                layoutMarketCheckBox.get(i).setSelected(false);
+            }
+            allSelected=false;
+        }
 
     }
 
-    /** Method allows to restart app */
+    /**
+     * Method allows to restart app
+     */
     public void restartApp() throws IOException {
         Stage stage = (Stage) restartButton.getScene().getWindow();
         stage.close();
@@ -157,7 +198,9 @@ public class Controller {
 
     }
 
-    /** Method allows to close app */
+    /**
+     * Method allows to close app
+     */
     public void closeApp() throws IOException {
         Stage stage = (Stage) exitButton.getScene().getWindow();
         stage.close();
